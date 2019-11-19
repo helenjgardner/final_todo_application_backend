079a0cc59fe60fb91894e4a73be4d74a9042dd9b
helenjgardner <helen_gardner@hotmail.com>
2019-11-19 11:13:35 +0000

    added put functionality

--- handler.js
+++ handler.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 app.get("/tasks",function(request,response){
    response.status(200).send({
      tasks: [
-       {id: 1, text:"Clean the car", completed: false, dateDue:"2019-11-30"},
-       {id: 2, text:"buy lunch", completed: false, dateDue:"2019-11-29"},
-       {id: 3, text:"cut hair", completed: true, dateDue:"2019-11-11"},
+       {id: 1, text:"Clean the car", completed: false, dateDue:"2019-11-30", dateDone:""},
+       {id: 2, text:"buy lunch", completed: false, dateDue:"2019-11-29", dateDone:""},
+       {id: 3, text:"cut hair", completed: true, dateDue:"2019-11-11", dateDone:"2019-11-19"}
      ]
    });
 });
@@ -22,16 +22,26 @@ app.get("/tasks",function(request,response){
 app.delete("/tasks/:taskId", function(request,response){
   // delete task
   const id=request.params.taskId;
-  response.status(200).send("Received request to delete task" +id);
+  response.status(200).send("Received request to delete task " + id);
 });
 
 app.post("/tasks", function(request, response){
    // new task
    const task=request.body;
-   response.status(201).send("created a new task with " + task.text);
+   response.status(201).send("Created a new task with " + task.text);
 });
-// add put echoing something back - maybe all things
-// add new repo and push
 
-
+// my fields from state in app.js
+// text
+// completed
+// dateDone
+// dateDue
+// id
+// possibly userid
+app.put("/tasks/:taskId", function(request,response){
+  // update task
+  const task=request.body;
+  const id=request.params.taskId;
+  response.status(200).send("Received a request to update task " + id + " with " + task.text + task.completed)
+});
 module.exports.tasks=serverless(app);
